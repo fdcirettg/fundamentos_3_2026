f5cba244f4ed4c29c3f13b33ee7d7c168955dd94
fdcirettg <federico.cirett@unison.mx>
2026-08-31 12:47:22 -0700

    added NombresArrayList.java

---
+++ NombresArrayList/NombresArrayList.java
@@ -0,0 +1,10 @@
+package fd_3.fundamentos_3_2026.NombresArrayList;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NombresArrayList {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("listado.txt");
+    }
+}
