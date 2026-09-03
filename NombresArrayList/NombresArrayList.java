4510da9c98decfbabb0a553b370040390df9163e
fdcirettg <federico.cirett@unison.mx>
2026-09-03 12:36:42 -0700

    Count fisrt names

--- NombresArrayList/NombresArrayList.java
+++ NombresArrayList/NombresArrayList.java
@@ -6,7 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.Stream;
-
+class nameCount {
+    String name;
+    int count;
+    public nameCount(String name) {
+        this.name = name;
+        this.count = 1;
+    }
+    public void incrementCount() {
+        this.count++;
+    }
+    public String getName() {
+        return this.name+" (" + this.count + ")";
+    }
+}
 public class NombresArrayList {
     public static void main(String[] args) {
         Path filePath = Paths.get("/Users/federicocirettgalan/Documents/proyectos_vscode/fundamentos_computacion3/fd_3/fundamentos_3_2026/NombresArrayList/listado.txt");
@@ -34,6 +47,26 @@ public class NombresArrayList {
         }
     }
     public static ArrayList<String> countNames(ArrayList<String> names) {
-        
+        ArrayList<nameCount> countedNames = new ArrayList<>();
+        int pos = 0; // track the position of the name 
+        for (String name : names) {
+            String[] splitName = name.split(" ");
+            boolean found = false;
+            for (nameCount nc : countedNames) {
+                if (nc.name.equals(splitName[pos])) {
+                    nc.incrementCount();
+                    found = true;
+                    break;  
+                }
+            }
+            if (!found) {
+                countedNames.add(new nameCount(splitName[pos]));
+            }
+        }
+        ArrayList<String> result = new ArrayList<>();
+        for (nameCount nc : countedNames) {
+            result.add(nc.getName());
+        }
+        return result;
     }
 }
