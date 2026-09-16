a2d3b9efbb33969614c2a2877cde04ef0099293a
fdcirettg <federico.cirett@unison.mx>
2026-09-15 17:08:55 -0700

    Last changes to Rover

--- Rover/src/Rover.java
+++ Rover/src/Rover.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class Rover {
     public static void main(String[] args) {
@@ -12,6 +15,31 @@ public class Rover {
         ArrayList<String> lineas = leerArchivo(cadena);
         Queue<String> queue = addToQueue(lineas);
         travelQueue(queue);
+        queue = addToQueue(lineas); // Recreate the queue since it was emptied in travelQueue
+        // Convert the queue to a stack and then back to a queue
+        Deque<String> stack = queueToStack(queue);
+        queue = stackToQueue(stack); 
+        travelQueue(queue);
+    }
+    public static Queue<String> stackToQueue(Deque<String> stack) {
+        Queue<String> queue = new LinkedList<String>();
+        while (!stack.isEmpty()) {
+            String linea = stack.pop();
+            if (linea != null) {
+                queue.add(linea);
+            }
+        }
+        return queue;
+    }
+    public static Deque<String> queueToStack(Queue<String> queue) {
+        Deque<String> deque = new ArrayDeque<>();
+        while (!queue.isEmpty()) {
+            String linea = queue.poll();
+            if (linea != null) {
+                deque.push(linea);
+            }
+        }
+        return deque;
     }
     public static void travelQueue(Queue<String> queue) {
         while (!queue.isEmpty()) {
